dc941b7aa1072fd9461d35df68a9ee4d2322650c
石川雄一 <>
2019-08-29 23:14:52 +0900

    いらないメソッドを削除

--- app/DailyReport.php
+++ app/DailyReport.php
@@ -9,9 +9,4 @@ class DailyReport extends Model
     protected $fillable = ['title', 'content', 'reporting_time', 'user_id', 'deleted_at'];
 
     protected $dates = ['reporting_time'];
-
-    public function getAll($date)
-    {
-        return $this->where('reporting_time', $date)->get();
-    }
 }
