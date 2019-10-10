c14bcb523e6c23e51e19c7bcdfa8f51c4dcbb4be
石川雄一 <>
2019-10-10 12:48:05 +0900

    バリデーションエラー時のリダイレクト処理を$rediretプロパティを使った処理に変更、if文の条件式をリクエストクラスのメソッドを使ったものに変更

--- app/Http/Requests/User/QuestionsRequest.php
+++ app/Http/Requests/User/QuestionsRequest.php
@@ -6,8 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class QuestionsRequest extends FormRequest
 {
-    protected $redirectRoute;
-
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -27,9 +25,9 @@ class QuestionsRequest extends FormRequest
     {
         $inputWord = $this->all();
         if (isset($inputWord['id'])) {
-            $this->redirectRoute = 'question.edit';
-        } elseif (array_key_exists('title', $inputWord) and array_key_exists('content', $inputWord)) {
-            $this->redirectRoute = 'question.create';
+            $this->redirect = 'question/'. $input['id']. '/edit';
+        } elseif ($this->has(['title', 'content'])) {
+            $this->redirect = 'question/create';
         }
         return [
             'select_tag_category_id' => ['sometimes', 'integer', 'nullable'],
@@ -47,29 +45,5 @@ class QuestionsRequest extends FormRequest
             'required' => '入力必須です',
             'max' => ':max以下で入力してください',
         ];
-    }
-
-    protected function getRedirectUrl()
-    {
-        $inputWord = $this->all();
-        $url = $this->redirector->getUrlGenerator();
-
-        switch ($this->redirectRoute) {
-            case 'question.edit':
-                return $url->route($this->redirectRoute, $inputWord['id']);
-                break;
-            default:
-                break;
-        }
-
-        if ($this->redirect) {
-            return $url->to($this->redirect);
-        } elseif ($this->redirectRoute) {
-            return $url->route($this->redirectRoute);
-        } elseif ($this->redirectAction) {
-            return $url->action($this->redirectAction);
-        }
-
-        return $url->previous();
     }
 }
